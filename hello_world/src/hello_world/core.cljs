0c5d59e47f4f110ba5c117eac54aa7738ca7ed1b
Jason Walsh <j@wal.sh>
2015-04-18 11:14:08 -0700

    Alternative logging syntax

--- hello_world/src/hello_world/core.cljs
+++ hello_world/src/hello_world/core.cljs
@@ -1,4 +1,11 @@
 (ns hello-world.core)
 
+(. js/console (log "Hello World! 1"))
 
-(. js/console (log "Hello World!"))
+(.log js/console "Hello World! 2")
+
+(js/console.log "Hello World! 3")
+
+(enable-console-print!)
+
+(println "Hello World! 4")
